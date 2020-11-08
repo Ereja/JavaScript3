9bc713cc770ed49c625586205b52baf39cb5eeaf
Vaida <93vaida@gmail.com>
2020-11-08 20:54:38 +0100

    rewrote code using template literals

--- hackyourrepo-app/util/contributors.js
+++ hackyourrepo-app/util/contributors.js
@@ -7,25 +7,15 @@ export function fetchContributors(query) {
     //making sure that with each click on select menu, new contributors being generated and not stacked on top of each other
     divForContr.innerHTML = '';
     contributorsData.forEach(contributor => {
-      //creating the elements for each contributor
-      const div = document.createElement('div');
-      const link = document.createElement('a');
-      const image = document.createElement('img');
-      const text = document.createElement('p');
-      const span = document.createElement('span');
-      //appending the elements
-      div.className = 'contributors-container';
-      divForContr.appendChild(div);
-      div.appendChild(image);
-      div.appendChild(text);
-      div.appendChild(span);
-      text.appendChild(link);
-
-      image.src = contributor.avatar_url;
-      image.alt = contributor.login;
-      link.innerText = contributor.login;
-      link.href = contributor.html_url;
-      span.innerText = contributor.contributions;
+      divForContr.innerHTML += `
+      <div class="contributors-container">
+      <img src="${contributor.avatar_url}" alt="${contributor.login}">
+      <p>
+      <a href="${contributor.html_url}" target_blank>${contributor.login}</a>
+      </p>
+      <span>${contributor.contributions}</span>
+      </div>
+      `;
     });
   });
 }
