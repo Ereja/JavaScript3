c26d343f72317a03a3e6d357757bbc55c94d70a9
Vaida <93vaida@gmail.com>
2020-11-09 16:38:00 +0100

    made sure that only 5 contributors being displayed at a time

--- hackyourrepo-app/util/contributors.js
+++ hackyourrepo-app/util/contributors.js
@@ -1,15 +1,19 @@
-//not sure why, even though fetchData is populated in main script.js file, I can access it here only after importing. Not sure if this would be considered good practise
 import { fetchData } from './repositoriesFetch.js';
 
 //This function uses fetchData in order to create layout for each contributor
-
-export function fetchContributors(query) {
+export function fetchContributors(query, rows_per_page, page) {
   fetchData(
     `https://api.github.com/repos/HackYourFuture/${query}/contributors`,
   ).then(contributorsData => {
     //making sure that with each click on select menu, new contributors being generated and not stacked on top of each other
     divForContr.innerHTML = '';
-    contributorsData.forEach(contributor => {
+    page--;
+
+    let start = rows_per_page * page;
+    let end = start + rows_per_page;
+    let paginatedContributors = contributorsData.slice(start, end);
+
+    paginatedContributors.forEach(contributor => {
       divForContr.innerHTML += `
       <div class="contributors-container">
       <img src="${contributor.avatar_url}" alt="${contributor.login}">
