abeb4c224136c2d22843705f1fb516ec05a31f19
Vaida <93vaida@gmail.com>
2020-11-10 11:48:37 +0100

    added error handling

--- hackyourrepo-app/util/repositoriesFetch.js
+++ hackyourrepo-app/util/repositoriesFetch.js
@@ -3,8 +3,12 @@
 export async function fetchData(url) {
   try {
     const response = await fetch(url);
+    if (response.ok) {
     const data = await response.json();
     return data;
+  } else {
+    throw new Error('Network rquest failed.');
+  }
   } catch (error) {
     body.innerHTML = error;
   }
