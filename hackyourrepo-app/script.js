7ba581f5f187bc335f8eefe685fa904cfecdbd0f
Vaida <93vaida@gmail.com>
2020-11-11 13:43:18 +0100

    seperated nested functions

--- hackyourrepo-app/script.js
+++ hackyourrepo-app/script.js
@@ -120,41 +120,43 @@ function main() {
         }
       }
       const option = selectMenu.value;
-      fetch(
-        `https://api.github.com/repos/HackYourFuture/${option}/contributors`,
-      )
-        .then(response => {
-          return response.json();
-        })
-        .then(contributorsData => {
-          //making sure that with each click on select menu, new contributors being generated and not stacked on top of each other
-          divForContr.innerHTML = '';
-          contributorsData.forEach(contributor => {
-            //creating the elements for each contributor
-            const div = document.createElement('div');
-            const link = document.createElement('a');
-            const image = document.createElement('img');
-            const text = document.createElement('p');
-            const span = document.createElement('span');
-            //appending the elements
-            div.className = 'contributors-container';
-            divForContr.appendChild(div);
-            div.appendChild(image);
-            div.appendChild(text);
-            div.appendChild(span);
-            text.appendChild(link);
-
-            image.src = contributor.avatar_url;
-            image.alt = contributor.login;
-            link.innerText = contributor.login;
-            link.href = contributor.html_url;
-            span.innerText = contributor.contributions;
-          });
-        })
-        .catch(error => {
-          console.log(error);
-        });
+      addContributorsInfo(option);
     });
+  }
+
+  function addContributorsInfo(option) {
+    fetch(`https://api.github.com/repos/HackYourFuture/${option}/contributors`)
+      .then(response => {
+        return response.json();
+      })
+      .then(contributorsData => {
+        //making sure that with each click on select menu, new contributors being generated and not stacked on top of each other
+        divForContr.innerHTML = '';
+        contributorsData.forEach(contributor => {
+          //creating the elements for each contributor
+          const div = document.createElement('div');
+          const link = document.createElement('a');
+          const image = document.createElement('img');
+          const text = document.createElement('p');
+          const span = document.createElement('span');
+          //appending the elements
+          div.className = 'contributors-container';
+          divForContr.appendChild(div);
+          div.appendChild(image);
+          div.appendChild(text);
+          div.appendChild(span);
+          text.appendChild(link);
+
+          image.src = contributor.avatar_url;
+          image.alt = contributor.login;
+          link.innerText = contributor.login;
+          link.href = contributor.html_url;
+          span.innerText = contributor.contributions;
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 } // <--end of main function
 
